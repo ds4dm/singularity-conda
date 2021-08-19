ee06850c98b779c86c65e52338e73b66f23c975e
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-08-19 09:03:20 -0400

    Custom SCIP and PySCIPOpt

--- post.bash
+++ post.bash
@@ -1,5 +1,31 @@
 # Fail fast
 set -o errexit
 
-# Fill this file with your environment creation
-mamba create --quiet --channel conda-forge --name project-name python=3.9 numpy
+# Download and extract SCIP
+readonly scip_version="7.0.2"
+readonly scip_filename="scipoptsuite-${scip_version}"
+wget "https://scipopt.org/download/release/${scip_filename}.tgz"
+tar -xzf "${scip_filename}.tgz"
+
+# Install conda packages
+mamba create --quiet --yes --name l2c --channel conda-forge \
+	compilers cmake make gmp zlib \
+	python=3.9 pip cython matplotlib pandas scipy \
+	ipython
+conda activate l2c
+
+# Missing flag for building SCIP
+export LDFLAGS="-lrt ${LDFLAGS}"
+
+# Build and install SCIP
+cmake -B build -S "${scip_filename}" \
+	-D CMAKE_BUILD_TYPE=Release \
+	-D PARASCIP=ON \
+	-D PAPILO=OFF \
+	-D GCG=OFF \
+	-D ZIMPL=OFF \
+	-D BOOST=OFF \
+	-D IPOPT=OFF
+cmake --build build --parallel
+cmake --install build --prefix "${CONDA_PREFIX}"
+pip install git+https://github.com/ds4dm/PySCIPOpt.git@gz
